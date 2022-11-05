3d15ad0f0902691f7ac8942c913e7f6001289c55
Guru18650 <guruwszystkiego@gmail.com>
2022-11-05 19:49:55 +0100

    bugfix

--- bot.py
+++ bot.py
@@ -1,19 +1,8 @@
 from genericpath import isfile
-import json 
+import json, time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException       
-import time
-
-def check_exists_by_xpath(xpath):
-        return driver.find_element(By.XPATH, xpath).is_displayed()
-
-def check_exists_by_xpath2(xpath):
-    try:
-        driver.find_element(By.XPATH, xpath)
-    except NoSuchElementException:
-        return False
-    return True
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException     
 
 if(isfile('login.json') == False):
     c = {"login":"","pass":""}
@@ -44,36 +33,43 @@ password_box.send_keys(u["pass"])
 s[1].click()
 a = driver.find_elements(By.TAG_NAME, "a")
 a[2].click()
-if(check_exists_by_xpath("//*[@id=\"continue_session_page\"]/div[1]")):
+if(driver.find_element(By.XPATH, "//*[@id=\"continue_session_page\"]/div[1]").is_displayed()):
     s2 = driver.find_element(By.XPATH, "//*[@id=\"continue_session_button\"]/h4")
 else:
     s2 = driver.find_element(By.XPATH, "//*[@id=\"start_session_button\"]/h4")
 s2.click()
 while 1==1:
-    time.sleep(0.2)
-    t = driver.find_element(By.XPATH, "//*[@id=\"question\"]/div[2]/div[2]").text
-    answer = driver.find_element(by=By.XPATH, value="//*[@id=\"answer\"]")
-    a = data.get(t, "0")
-    if(a=="0"):
-        if(check_exists_by_xpath("//*[@id=\"dont_know_new\"]")):
-            case3 = driver.find_element(by=By.XPATH, value="//*[@id=\"dont_know_new\"]")
-            case3.click()
-            case4 = driver.find_element(by=By.XPATH, value="//*[@id=\"skip\"]")
-            case4.click()
+    try:
+        time.sleep(0.2)
+        t = driver.find_element(By.XPATH, "//*[@id=\"question\"]/div[2]/div[2]").text
+        answer = driver.find_element(by=By.XPATH, value="//*[@id=\"answer\"]")
+        a = data.get(t, "0")
+        if(a=="0"):
+            if(driver.find_element(By.XPATH,"//*[@id=\"dont_know_new\"]").is_displayed()):
+                case3 = driver.find_element(by=By.XPATH, value="//*[@id=\"dont_know_new\"]")
+                case3.click()
+                case4 = driver.find_element(by=By.XPATH, value="//*[@id=\"skip\"]")
+                case4.click()
+            else:
+                case = driver.find_element(by=By.XPATH, value="//*[@id=\"check\"]")
+                case.click()
+                time.sleep(0.2)
+                word = driver.find_element(by=By.XPATH, value="//*[@id=\"word\"]").text
+                data[t] = word
+                json_object = json.dumps(data, indent=4, ensure_ascii=False)
+                with open("data.json", "w", encoding='utf-8') as outfile:
+                    outfile.write(json_object)
+                case3 = driver.find_element(by=By.XPATH, value="//*[@id=\"nextword\"]")
+                case3.click()
         else:
+            answer.send_keys(a)
             case = driver.find_element(by=By.XPATH, value="//*[@id=\"check\"]")
             case.click()
-            time.sleep(0.2)
-            word = driver.find_element(by=By.XPATH, value="//*[@id=\"word\"]").text
-            data[t] = word
-            json_object = json.dumps(data, indent=4, ensure_ascii=False)
-            with open("data.json", "w", encoding='utf-8') as outfile:
-                outfile.write(json_object)
-            case3 = driver.find_element(by=By.XPATH, value="//*[@id=\"nextword\"]")
-            case3.click()
-    else:
-        answer.send_keys(a)
-        case = driver.find_element(by=By.XPATH, value="//*[@id=\"check\"]")
-        case.click()
-        case2 = driver.find_element(by=By.XPATH, value="//*[@id=\"nextword\"]")
-        case2.click()
+            case2 = driver.find_element(by=By.XPATH, value="//*[@id=\"nextword\"]")
+            case2.click()
+    except ElementNotInteractableException:
+        driver.close()
+        exit()
+    except NoSuchElementException:
+        driver.close()
+        exit()
