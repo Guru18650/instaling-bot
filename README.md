694ed9c33e6cf84852976c2e826d1d9625cc938f
Guru18650 <guruwszystkiego@gmail.com>
2022-11-05 19:31:21 +0100

    self learining and fixes

--- README.md
+++ README.md
@@ -15,19 +15,12 @@ Install dependencies with pip
   pip install selenium
   pip install webdriver_manager
 ```
-Fill data.json with your translated words (example data is included). Both words and translations need to be 100% accurate or bot will loop and crash. data.json needs to have all possible words. 
-
 ## Usage
 
 ```python
 py bot.py
 ```
-If it's the first time running it, it will ask for Instaling login and password. Later it will save it in login.json. If you want to change those, edit this json or delete it. bot.py will ask for credentials again.
-
-Wait until bot will finish and crash (sadly)
-
-IMPORTANT: the session can't be started before running bot (it searches for "start_session_button" and not "continue_session_button"). You can change it in line 29 of bot.py, but remember to change it back when its done.
-
+If it's the first time running it, it will ask for Instaling login and password. Later it will save it in login.json. If you want to change those, edit this json or delete it. bot.py will ask for credentials again. Data will be learnt by bruteforce, data.json will be generated. There you can find all gathered translations ("your language":"foreign language"). You can see that bot makes mistakes at first few runs, thats because it is learning new words. Later it will be 100% accurate. 
 
 
 
--- bot.py
+++ bot.py
@@ -2,7 +2,19 @@ from genericpath import isfile
 import json 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException       
 import time
+
+def check_exists_by_xpath(xpath):
+        return driver.find_element(By.XPATH, xpath).is_displayed()
+
+def check_exists_by_xpath2(xpath):
+    try:
+        driver.find_element(By.XPATH, xpath)
+    except NoSuchElementException:
+        return False
+    return True
+
 if(isfile('login.json') == False):
     c = {"login":"","pass":""}
     print("Podaj login")
@@ -13,8 +25,14 @@ if(isfile('login.json') == False):
         json.dump(c, outfile)
 u = open('login.json', encoding='utf-8')
 u = json.load(u)
-f = open('data.json', encoding='utf-8')
-data = json.load(f)
+if(isfile('data.json') == False):
+    data = {}
+    with open("data.json", "w") as outfile:
+        json.dump(data, outfile)
+
+else:
+    f = open('data.json', encoding='utf-8')
+    data = json.load(f)
 driver = webdriver.Chrome()
 driver.get("https://instaling.pl/teacher.php?page=login")
 driver.implicitly_wait(0.5)
@@ -26,7 +44,10 @@ password_box.send_keys(u["pass"])
 s[1].click()
 a = driver.find_elements(By.TAG_NAME, "a")
 a[2].click()
-s2 = driver.find_element(By.XPATH, "//*[@id=\"start_session_button\"]/h4")
+if(check_exists_by_xpath("//*[@id=\"continue_session_page\"]/div[1]")):
+    s2 = driver.find_element(By.XPATH, "//*[@id=\"continue_session_button\"]/h4")
+else:
+    s2 = driver.find_element(By.XPATH, "//*[@id=\"start_session_button\"]/h4")
 s2.click()
 while 1==1:
     time.sleep(0.2)
@@ -34,10 +55,22 @@ while 1==1:
     answer = driver.find_element(by=By.XPATH, value="//*[@id=\"answer\"]")
     a = data.get(t, "0")
     if(a=="0"):
-        case3 = driver.find_element(by=By.XPATH, value="//*[@id=\"dont_know_new\"]")
-        case3.click()
-        case4 = driver.find_element(by=By.XPATH, value="//*[@id=\"skip\"]")
-        case4.click()
+        if(check_exists_by_xpath("//*[@id=\"dont_know_new\"]")):
+            case3 = driver.find_element(by=By.XPATH, value="//*[@id=\"dont_know_new\"]")
+            case3.click()
+            case4 = driver.find_element(by=By.XPATH, value="//*[@id=\"skip\"]")
+            case4.click()
+        else:
+            case = driver.find_element(by=By.XPATH, value="//*[@id=\"check\"]")
+            case.click()
+            time.sleep(0.2)
+            word = driver.find_element(by=By.XPATH, value="//*[@id=\"word\"]").text
+            data[t] = word
+            json_object = json.dumps(data, indent=4, ensure_ascii=False)
+            with open("data.json", "w", encoding='utf-8') as outfile:
+                outfile.write(json_object)
+            case3 = driver.find_element(by=By.XPATH, value="//*[@id=\"nextword\"]")
+            case3.click()
     else:
         answer.send_keys(a)
         case = driver.find_element(by=By.XPATH, value="//*[@id=\"check\"]")
